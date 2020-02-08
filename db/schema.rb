648be2d82e8f2ae0abbf426bdbe603b16278b1b4
rosemarie betrong <rmbt.lab@gmail.com>
2020-02-08 15:22:41 -0500

    Add Bio to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_201722) do
+ActiveRecord::Schema.define(version: 2020_02_08_202206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_201722) do
     t.string "username"
     t.string "user_type"
     t.string "location"
+    t.text "user_bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
