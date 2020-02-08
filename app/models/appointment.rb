08636818016ec134a3aa4e4c58cb19a51302d343
mickael merle <mickaelmerle6901@gmail.com>
2020-02-08 17:02:30 -0500

    add appointment model + dependencies

---
+++ app/models/appointment.rb
@@ -0,0 +1,5 @@
+class Appointment < ApplicationRecord
+  belongs_to :service
+  belongs_to :user
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_212827) do
+ActiveRecord::Schema.define(version: 2020_02_08_214155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "date"
+    t.string "status"
+    t.integer "rating"
+    t.bigint "service_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["service_id"], name: "index_appointments_on_service_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "services", force: :cascade do |t|
     t.string "type"
@@ -44,5 +56,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_212827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "services"
+  add_foreign_key "appointments", "users"
   add_foreign_key "services", "users"
 end
