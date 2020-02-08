ed247d0800f021e5569a4b0e20842d0ceacbb3db
rosemarie betrong <rmbt.lab@gmail.com>
2020-02-08 16:07:53 -0500

    Add Specialty to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_202206) do
+ActiveRecord::Schema.define(version: 2020_02_08_210650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_202206) do
     t.string "user_type"
     t.string "location"
     t.text "user_bio"
+    t.string "specialty"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
