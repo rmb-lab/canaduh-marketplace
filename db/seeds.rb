f45c17224f7ab253b48ddd752bee53a7afaf95e1
mickael merle <mickaelmerle6901@gmail.com>
2020-02-11 19:04:50 -0500

    seeding in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ first_name: 'Mickael'}, {last_name: 'Merle'}, {email: 'mickael@gmail.com'}])
+users = User.create([{ first_name: 'Rose Marie'}, {last_name: 'Be Trong'}, {email: 'rosemarie@gmail.com'}])
+users = User.create([{ first_name: 'Patrick'}, {last_name: 'Bourget'}, {email: 'patrick@gmail.com'}])
+users = User.create([{ first_name: 'Belis'}, {last_name: 'Turegon'}, {email: 'belis@gmail.com'}])
+
