fadf1128490fe30022170ba4f8f707a9b1cc7bcc
Belis Turegun <trgnb6@gmail.com>
2020-02-13 21:08:03 -0500

    added time & hour to booking form

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,22 +2,24 @@ class AppointmentsController < ApplicationController
   before_action :set_service, only: %i(create)
   # NEW: In the service controller? #
   # GET #
-  # def new
-  #   @service = Service.find(params[:service_id])
-  #   @appointment = Appointment.new
-  # end
+  def new
+    @service = Service.find(params[:service_id])
+    @appointment = Appointment.new
+  end
 
   # POST #
-  # def create
-  #   @appointment = Appointment.new(appointment_params)
-  #   @service = Service.find(params[:appointment][:service_id])
-  #   @appointment.service = @service
-  #   if @appointment.save
-  #     redirect_to service_path(@appointment)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @service = Service.find(params[:service_id])
+    @appointment.service = @service
+    @appointment.user = current_user
+    @appointment.status = 'pending confirmation'
+    if @appointment.save
+      redirect_to dashboard_path
+    else
+      redirect_to service_path(@service)
+    end
+  end
 
   # DELETE #
   # def destroy
@@ -28,11 +30,11 @@ class AppointmentsController < ApplicationController
 
   private
 
-  # def set_service
-  #   @service = Service.find(params[:service_id])
-  # end
+  def set_service
+    @service = Service.find(params[:service_id])
+  end
 
-  # def appointment_params
-  #   params.require(:appointment).permit(:date, :status, :service_id, :user_id, :rating)
-  # end
+  def appointment_params
+    params.require(:appointment).permit(:date, :status, :service_id, :user_id, :rating)
+  end
 end
