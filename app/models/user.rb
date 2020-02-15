1370c6e1e5539a951c1d77a292edf07354295b47
rosemarie betrong <rmbt.lab@gmail.com>
2020-02-15 14:58:27 -0500

    seed change

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :appointments
-  has_many :services
+  has_many :appointments, dependent: :destroy
+  has_many :services, dependent: :destroy
   has_many :advisor_appointments, through: :services, source: :appointments
   # validates_inclusion_of :user_type, in: USER_TYPE_OPTIONS
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -188,19 +188,19 @@ puts 'Creating "Services"...'
 service1 = Service.create!(
   user: belis,
   service_type: "Ongoing Assistance",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh urna, viverra eu risus sit amet, suscipit luctus metus. Maecenas facilisis mauris non dui semper sodales. Proin a dolor elementum ligula pulvinar efficitur nec eget est. Mauris convallis ultricies justo a pellentesque. Aliquam vitae ultrices nisi, id faucibus sem. Vivamus vitae dignissim diam. Sed in dictum felis. Morbi sit amet.",
+  description: Faker::Quote.yoda,
   price: "60"
 )
 service2 = Service.create!(
   user: belis,
   service_type: "Test Prep",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh urna, viverra eu risus sit amet, suscipit luctus metus. Maecenas facilisis mauris non dui semper sodales. Proin a dolor elementum ligula pulvinar efficitur nec eget est. Mauris convallis ultricies justo a pellentesque. Aliquam vitae ultrices nisi, id faucibus sem. Vivamus vitae dignissim diam. Sed in dictum felis. Morbi sit amet.",
+  description: Faker::Quote.yoda,
   price: "30"
 )
 service3 = Service.create!(
   user: belis,
   service_type: "General Consultation",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh urna, viverra eu risus sit amet, suscipit luctus metus. Maecenas facilisis mauris non dui semper sodales. Proin a dolor elementum ligula pulvinar efficitur nec eget est. Mauris convallis ultricies justo a pellentesque. Aliquam vitae ultrices nisi, id faucibus sem. Vivamus vitae dignissim diam. Sed in dictum felis. Morbi sit amet.",
+  description: Faker::Quote.yoda,
   price: "20"
 )
 
