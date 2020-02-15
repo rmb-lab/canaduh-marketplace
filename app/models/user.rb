882401f3ead72e82293f591f6dbca503321b9d08
Belis Turegun <trgnb6@gmail.com>
2020-02-15 14:52:59 -0500

    added new service

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
   has_many :services
   has_many :advisor_appointments, through: :services, source: :appointments
   # validates_inclusion_of :user_type, in: USER_TYPE_OPTIONS
-
 end
