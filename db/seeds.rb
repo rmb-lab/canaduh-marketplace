2b7de34205a1532739726845efc3b3991e592952
Patrick Bourget <patrickmbourget@gmail.com>
2020-02-15 12:24:06 -0500

    added path to services model and updated index/show to reflect .path

--- db/seeds.rb
+++ db/seeds.rb
@@ -188,18 +188,21 @@ puts 'Creating "Services"...'
 service1 = Service.create!(
   user: belis,
   service_type: "Ongoing Assistance",
+  path: "Student Visa",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh urna, viverra eu risus sit amet, suscipit luctus metus. Maecenas facilisis mauris non dui semper sodales. Proin a dolor elementum ligula pulvinar efficitur nec eget est. Mauris convallis ultricies justo a pellentesque. Aliquam vitae ultrices nisi, id faucibus sem. Vivamus vitae dignissim diam. Sed in dictum felis. Morbi sit amet.",
   price: "60"
 )
 service2 = Service.create!(
   user: belis,
   service_type: "Test Prep",
+  path: "Work Visa",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh urna, viverra eu risus sit amet, suscipit luctus metus. Maecenas facilisis mauris non dui semper sodales. Proin a dolor elementum ligula pulvinar efficitur nec eget est. Mauris convallis ultricies justo a pellentesque. Aliquam vitae ultrices nisi, id faucibus sem. Vivamus vitae dignissim diam. Sed in dictum felis. Morbi sit amet.",
   price: "30"
 )
 service3 = Service.create!(
   user: belis,
   service_type: "General Consultation",
+  path: "Canadian Citizenship",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh urna, viverra eu risus sit amet, suscipit luctus metus. Maecenas facilisis mauris non dui semper sodales. Proin a dolor elementum ligula pulvinar efficitur nec eget est. Mauris convallis ultricies justo a pellentesque. Aliquam vitae ultrices nisi, id faucibus sem. Vivamus vitae dignissim diam. Sed in dictum felis. Morbi sit amet.",
   price: "20"
 )
