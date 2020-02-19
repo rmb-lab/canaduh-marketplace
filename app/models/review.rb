3522c04c784b549796821f900e1812274674050c
mickael merle <mickaelmerle6901@gmail.com>
2020-02-18 20:23:43 -0500

    added review model, controller, view

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :service
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :review_date, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+end
